a5576be40c4ec59917288ff3b9e29c2decb510f7
Efren Gonzales <colorado3106@gmail.com>
2019-10-11 09:50:12 -0600

    snap challange 10-11

--- snap-10-11/erd.php
+++ snap-10-11/erd.php
@@ -8,6 +8,6 @@
 <body>
 
 <h1 align="center"><strong></strong><font size="6">Snap 10-11 ERD</font></h1>
-<p align="center"><img src="Snap%2010-11%20ERD.png">
+<p align="center"><img src="Snap%2010-11%20ERD1.png">
 </body>
 </html>
